2f911a709baa291f2d6e6f123fae97a701856c18
Khaif Mohamed <khaifmohamededava@gmail.com>
2025-07-01 20:05:53 +0530

    feat: completed auth

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,6 +1,5 @@
-'use client';
-
 import { GoogleIcon } from '@/assets/icons';
+import { signIn } from '@/auth';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import Image from 'next/image';
@@ -9,14 +8,16 @@ export default function LoginPage() {
   return (
     <main className="flex items-center justify-center h-svh bg-accent">
       <section className="flex-1 hidden items-center bg-gray-900 h-full text-white md:flex">
-       <div className='flex flex-col space-y-4  ml-28'>
-         <span className='flex items-center space-x-3'>
-          <Image src="/logo.svg" alt="Logo" width={40} height={40} className="size-10" />
-          <h2 className='font-medium text-2xl'>Landix</h2>
-        </span>
+        <div className="flex flex-col space-y-4  ml-28">
+          <span className="flex items-center space-x-3">
+            <Image src="/logo.svg" alt="Logo" width={40} height={40} className="size-10" />
+            <h2 className="font-medium text-2xl">Landix</h2>
+          </span>
 
-        <p className='text-sm text-muted-foreground'>Build you own landing pages with a prompt.</p>
-       </div>
+          <p className="text-sm text-muted-foreground">
+            Build you own landing pages with a prompt.
+          </p>
+        </div>
       </section>
 
       <section className="flex-1 flex items-center justify-center">
@@ -25,10 +26,20 @@ export default function LoginPage() {
             <CardHeader>
               <CardTitle>Login to Continue</CardTitle>
             </CardHeader>
-            <Button variant="outline" className="flex space-x-2">
-              Continue with Google
-              <GoogleIcon className="w-4 h-4 ml-2" />
-            </Button>
+            <form
+              action={async () => {
+                'use server';
+                await signIn('google', {
+                  redirectTo: '/',
+                });
+              }}
+              className="w-full"
+            >
+              <Button variant="outline" className="flex space-x-2 w-full">
+                Continue with Google
+                <GoogleIcon className="w-4 h-4 ml-2" />
+              </Button>
+            </form>
           </CardContent>
         </Card>
       </section>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
-export default function Home() {
+import { auth } from '@/auth';
+
+export default async function Home() {
+  const session = await auth();
+  console.log(session);
+  
   return <div>Home</div>;
 }
